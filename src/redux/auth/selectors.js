0d74c99ab8d4326563ae01d57478ff3bd0fe2415
Vahide Segah Üney <vahidesegah@gmail.com>
2025-03-03 20:53:10 +0300

    redux auth selectors export

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -3,3 +3,9 @@ export const selectLoading = (state) => state.tasks.loading;
 export const selectFilter = (state) => state.tasks.filter;
 
 export const selectAllTasks = (state) => state.tasks.items;
+
+export const selectIsRefreshing = (state) => state.auth.isRefreshing;
+
+export const selectIsloggedIn = (state) => state.auth.isLoggedIn;
+
+export const selectUser = (state) => state.auth.user;
