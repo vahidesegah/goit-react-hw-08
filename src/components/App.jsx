2d0c73551eeb72595ac30e7c10e514cf1dda09ee
Vahide Segah Üney <vahidesegah@gmail.com>
2025-03-08 12:50:33 +0300

    pages css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,43 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Routes, Route } from 'react-router-dom';
-import { refreshUser } from './redux/auth/operations';
-import Layout from './components/Layout/Layout';
-import HomePage from './pages/HomePage/HomePage';
-import RegistrationPage from './pages/RegistrationPage/RegistrationPage';
-import LoginPage from './pages/LoginPage/LoginPage';
-import ContactsPage from './pages/ContactsPage/ContactsPage';
-import PrivateRoute from './components/PrivateRoute/PrivateRoute';
-import RestrictedRoute from './components/RestrictedRoute/RestrictedRoute';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Routes, Route } from "react-router-dom";
+import { refreshUser } from "../redux/auth/operations";
+import Layout from "./Layout";
+import HomePage from "../pages/HomePage/HomePage";
+import RegisterPage from "../pages/RegisterPage/RegisterPage";
+import LoginPage from "../pages/LoginPage/LoginPage";
+import ContactsPage from "../pages/ContactsPage/ContactsPage";
+import PrivateRoute from "./PrivateRoute";
+import RestrictedRoute from "./RestrictedRoute";
 
 const App = () => {
-  const dispatch = useDispatch() : <p>Loading...</p>;
+  const dispatch = useDispatch();
   const { isRefreshing } = useSelector((state) => state.auth);
 
   useEffect(() => {
     dispatch(refreshUser());
   }, [dispatch]);
 
-  return (
-    !isRefreshing ? (
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="/register" element={<RestrictedRoute element={<RegistrationPage />} />} />
-          <Route path="/login" element={<RestrictedRoute element={<LoginPage />} />} />
-          <Route path="/contacts" element={<PrivateRoute element={<ContactsPage />} />} />
-        </Route>
-      </Routes>
-    )
+  return !isRefreshing ? (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route
+          path="/register"
+          element={<RestrictedRoute element={<RegisterPage />} />}
+        />
+        <Route
+          path="/login"
+          element={<RestrictedRoute element={<LoginPage />} />}
+        />
+        <Route
+          path="/contacts"
+          element={<PrivateRoute element={<ContactsPage />} />}
+        />
+      </Route>
+    </Routes>
+  ) : (
+    <p>Loading...</p>
   );
 };
 
