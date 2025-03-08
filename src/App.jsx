2fe3fddba7c33c0c220087c16b139c545522ef59
Vahide Segah Üney <vahidesegah@gmail.com>
2025-03-08 13:37:50 +0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Routes, Route } from "react-router-dom";
-import { refreshUser } from "../redux/auth/operations";
-import Layout from "./Layout";
-import HomePage from "../pages/HomePage/HomePage";
-import RegisterPage from "../pages/RegisterPage/RegisterPage";
-import LoginPage from "../pages/LoginPage/LoginPage";
-import ContactsPage from "../pages/ContactsPage/ContactsPage";
-import PrivateRoute from "./PrivateRoute";
-import RestrictedRoute from "./RestrictedRoute";
+import { refreshUser } from "./redux/auth/operations";
+import Layout from "./components/Layout";
+import HomePage from "./pages/HomePage/HomePage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import ContactsPage from "./pages/ContactsPage/ContactsPage";
+import PrivateRoute from "./components/PrivateRoute";
+import RestrictedRoute from "./components/RestrictedRoute";
 
-const App = () => {
+function App() {
   const dispatch = useDispatch();
   const { isRefreshing } = useSelector((state) => state.auth);
 
@@ -39,6 +39,6 @@ const App = () => {
   ) : (
     <p>Loading...</p>
   );
-};
+}
 
 export default App;
--- src/redux/contacts/selectors.js
+++ src/redux/contacts/selectors.js
@@ -1,5 +1,5 @@
-export const selectLoading = (state) => state.tasks.loading;
+export const selectLoading = (state) => state.contacts.loading;
 
-export const selectFilter = (state) => state.tasks.filter;
+export const selectFilter = (state) => state.contacts.filter;
 
-export const selectAllTasks = (state) => state.tasks.items;
+export const selectAllContacts = (state) => state.contacts.items;
